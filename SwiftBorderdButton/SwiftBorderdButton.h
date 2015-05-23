bc1da488084201409c617eb8fa3bf42c833cfb54
86 <triaedz@gmail.com>
2015-05-23 21:59:43 +0900

    Initial Commit

---
+++ SwiftBorderdButton/SwiftBorderdButton.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBorderdButton.h
+//  SwiftBorderdButton
+//
+//  Created by daisuke yamanaka on 2015/05/23.
+//  Copyright (c) 2015年 triaedz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBorderdButton.
+FOUNDATION_EXPORT double SwiftBorderdButtonVersionNumber;
+
+//! Project version string for SwiftBorderdButton.
+FOUNDATION_EXPORT const unsigned char SwiftBorderdButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBorderdButton/PublicHeader.h>
+
+
