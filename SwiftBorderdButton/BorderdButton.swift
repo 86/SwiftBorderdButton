75177d6ff7a9b95766f2df0977f46cf69311c08c
86 <triaedz@gmail.com>
2015-05-23 22:09:13 +0900

    setup

---
+++ SwiftBorderdButton/BorderdButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderdButton.swift
+//  SwiftBorderdButton
+//
+//  Created by daisuke yamanaka on 2015/05/23.
+//  Copyright (c) 2015年 triaedz. All rights reserved.
+//
+
+import UIKit
+
+class BorderdButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SwiftBorderdButtonDemo/SwiftBorderdButtonDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBorderdButtonDemo
+//
+//  Created by daisuke yamanaka on 2015/05/23.
+//  Copyright (c) 2015年 triaedz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
