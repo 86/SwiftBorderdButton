a5fabe4d22e74d0e67916569510803a1b61ccb1e
86 <triaedz@gmail.com>
2015-05-24 23:45:31 +0900

    implement to change colors when button is diable

--- SwiftBorderdButton/BorderdButton.swift
+++ SwiftBorderdButton/BorderdButton.swift
@@ -14,19 +14,38 @@ public class BorderdButton: UIButton {
     
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
-            layer.cornerRadius = cornerRadius
-            layer.masksToBounds = cornerRadius > 0
+                layer.cornerRadius = cornerRadius
+                layer.masksToBounds = cornerRadius > 0
         }
     }
+    
     @IBInspectable var borderWidth: CGFloat = 0 {
         didSet {
             layer.borderWidth = borderWidth
         }
     }
+    
     @IBInspectable var borderColor: UIColor? {
         didSet {
             layer.borderColor = borderColor?.CGColor
         }
     }
     
+    @IBInspectable var borderColorDisable: UIColor?
+    
+    public override var enabled: Bool {
+        didSet {
+            if enabled {
+                layer.borderColor = borderColor?.CGColor
+                layer.opacity = 1.0
+            } else {
+                if (borderColorDisable != nil) {
+                    layer.borderColor = borderColorDisable?.CGColor
+                } else {
+                    layer.borderColor = currentTitleColor.CGColor
+                }
+                layer.opacity = 0.4
+            }
+        }
+    }
 }
--- SwiftBorderdButtonDemo/SwiftBorderdButtonDemo/ViewController.swift
+++ SwiftBorderdButtonDemo/SwiftBorderdButtonDemo/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import SwiftBorderdButton
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var borderedButton1: BorderdButton!
+    @IBOutlet weak var borderedButton2: BorderdButton!
+    @IBOutlet weak var stateSwitch1: UISwitch!
+    @IBOutlet weak var stateSwitch2: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stateSwitch1.addTarget(self, action: "switchState:", forControlEvents: UIControlEvents.ValueChanged)
+        stateSwitch2.addTarget(self, action: "switchState:", forControlEvents: UIControlEvents.ValueChanged)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func switchState(sender: AnyObject) {
+        if sender .isEqual(stateSwitch1) {
+            if borderedButton1.enabled {
+                borderedButton1.enabled = false
+            } else {
+                borderedButton1.enabled = true
+            }
+        } else if sender .isEqual(stateSwitch2) {
+            if borderedButton2.enabled {
+                borderedButton2.enabled = false
+            } else {
+                borderedButton2.enabled = true
+            }
+        }
+    }
+    
 }
 
